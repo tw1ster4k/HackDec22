5353553ce52fceed13a32572b45a738f89345fde
Anastasiya <iinokayo@mail.ru>
2022-12-17 14:23:33 +0300

    Fire clone

--- src/component.js
+++ src/component.js
@@ -1,5 +1,6 @@
 const player = document.querySelector('.player');
 const fire = document.querySelector('.fire');
+const root = document.querySelector('.root');
 
 const playerHeight = player.clientHeight;
 const playerWidth = player.clientWidth;
@@ -11,22 +12,27 @@ const maxHeight = windowHeight - playerHeight;
 window.addEventListener('keydown', (event) => {
   switch (event.key) {
     case ' ':
-      // eslint-disable-next-line no-case-declarations
-      const x = player.offsetTop + playerHeight / 2 - fireHeight / 2;
-      // eslint-disable-next-line no-case-declarations
-      const y = playerWidth;
-      fire.style.visibility = 'visible';
-      fire.style.top = x + 'px';
-      fire.style.left = y + 'px';
-      setInterval(() => {
-        let plusLeft = fire.offsetLeft;
-        if (plusLeft < windowWidth - 20) {
-          plusLeft += 1;
-          fire.style.left = plusLeft + 'px';
-        } else {
-          fire.style.display = 'none';
-        }
-      }, 1);
+      const fireClone = fire.cloneNode(true);
+      // player.offsetTop - количество пикселей от верха экрана до верхней границы игрока
+      // playerHeight / 2 - середина высоты игрока
+      // fireHeight / 2 - середина элемента огня
+      console.log(fireClone);
+      console.log(player.offsetTop);
+      const fireX = player.offsetTop + playerHeight / 2;
+      // const x = player.offsetTop + playerHeight / 2 - fireHeight / 2;
+      // const y = playerWidth;
+      // fire.style.visibility = 'visible';
+      // fire.style.top = x + 'px';
+      // fire.style.left = y + 'px';
+      // setInterval(() => {
+      //   let plusLeft = fire.offsetLeft;
+      //   if (plusLeft < windowWidth - 20) {
+      //     plusLeft += 1;
+      //     fire.style.left = plusLeft + 'px';
+      //   } else {
+      //     fire.style.display = 'none';
+      //   }
+      // }, 1);
       break;
     case 'ArrowUp':
       if (player.offsetTop >= 0) {
