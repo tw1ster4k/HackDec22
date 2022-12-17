d37ace834463c98158156d9af36ca9bcdd61c5f7
Murzabulatov <slvtz@icloud.com>
2022-12-17 15:10:04 +0500

    test relese

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+// test comment
 const app = express();
 
 app.listen(3000).on('error', (error) => {console.log(error)}).on('listening', () => {console.log('Success')})
