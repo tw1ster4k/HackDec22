6f28f6c3eb0ea6cc6707b51e196d9db1b798410f
Anastasiya <iinokayo@mail.ru>
2022-12-17 16:03:14 +0300

    Added enemies

--- src/component.js
+++ src/component.js
@@ -1,38 +1,44 @@
 const player = document.querySelector('.player');
 const fire = document.querySelector('.fire');
 const root = document.querySelector('.root');
+const enemy = document.querySelector('.enemy');
 
 const playerHeight = player.clientHeight;
 const playerWidth = player.clientWidth;
-const fireHeight = fire.clientHeight;
+
 const windowHeight = document.documentElement.clientHeight;
 const windowWidth = document.documentElement.clientWidth;
+
 const maxHeight = windowHeight - playerHeight;
+const fireHeight = fire.clientHeight;
+const enemyHeight = enemy.clientHeight;
 
 window.addEventListener('keydown', (event) => {
   switch (event.key) {
     case ' ':
       const fireClone = fire.cloneNode(true);
+      root.appendChild(fireClone);
       // player.offsetTop - количество пикселей от верха экрана до верхней границы игрока
       // playerHeight / 2 - середина высоты игрока
       // fireHeight / 2 - середина элемента огня
-      console.log(fireClone);
-      console.log(player.offsetTop);
-      const fireX = player.offsetTop + playerHeight / 2;
-      // const x = player.offsetTop + playerHeight / 2 - fireHeight / 2;
-      // const y = playerWidth;
-      // fire.style.visibility = 'visible';
-      // fire.style.top = x + 'px';
-      // fire.style.left = y + 'px';
-      // setInterval(() => {
-      //   let plusLeft = fire.offsetLeft;
-      //   if (plusLeft < windowWidth - 20) {
-      //     plusLeft += 1;
-      //     fire.style.left = plusLeft + 'px';
-      //   } else {
-      //     fire.style.display = 'none';
-      //   }
-      // }, 1);
+      const fireX = player.offsetTop + playerHeight / 2 - fireHeight / 2;
+      const fireY = playerWidth;
+      fireClone.style.visibility = 'visible';
+      fireClone.style.top = fireX + 'px';
+      fireClone.style.left = fireY + 'px';
+      setInterval(() => {
+        let goRight = fireClone.offsetLeft;
+        if (goRight < windowWidth - 20) {
+          goRight += 1;
+          fireClone.style.left = goRight + 'px';
+        } else {
+          fireClone.remove();
+        }
+        const enemiesList = Array.from(document.querySelectorAll('.enemy'));
+        console.log(enemiesList);
+        const fireList = Array.from(document.querySelectorAll('.fire'));
+        console.log(fireList);
+      }, 10);
       break;
     case 'ArrowUp':
       if (player.offsetTop >= 0) {
@@ -50,3 +56,24 @@ window.addEventListener('keydown', (event) => {
       break;
   }
 });
+
+const getRandomHeight = (elHeight) => {
+  return Math.floor(Math.random() * (windowHeight - elHeight - 50) - 50);
+};
+enemy.style.top = getRandomHeight(enemyHeight) + 'px';
+
+setInterval(() => {
+  const enemyClone = enemy.cloneNode(true);
+  enemyClone.style.visibility = 'visible';
+  enemyClone.style.top = getRandomHeight(enemyHeight) + 'px';
+  root.appendChild(enemyClone);
+  setInterval(() => {
+    let goLeft = enemyClone.offsetLeft;
+    if (enemyClone.offsetLeft > 0) {
+      goLeft -= 1;
+      enemyClone.style.left = goLeft + 'px';
+    } else {
+      enemyClone.remove();
+    }
+  }, 10);
+}, 2000);
