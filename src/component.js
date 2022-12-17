ecea1bde68debf263eb2d62989b182efdacaac47
Anastasiya <iinokayo@mail.ru>
2022-12-17 12:32:16 +0300

    Ship fly and shoot

--- src/component.js
+++ src/component.js
@@ -1,14 +1,32 @@
 const player = document.querySelector('.player');
+const fire = document.querySelector('.fire');
+
+const playerHeight = player.clientHeight;
+const playerWidth = player.clientWidth;
+const fireHeight = fire.clientHeight;
+const windowHeight = document.documentElement.clientHeight;
+const maxHeight = windowHeight - playerHeight;
 
 window.addEventListener('keydown', (event) => {
   switch (event.key) {
+    case ' ':
+      const x = player.offsetTop + playerHeight / 2 - fireHeight / 2;
+      const y = playerWidth;
+      fire.style.visibility = 'visible';
+      fire.style.top = x + 'px';
+      fire.style.left = y + 'px';
+      break;
     case 'ArrowUp':
-      const goUp = player.offsetTop;
-      player.style.top = goUp - 5 + 'px';
+      if (player.offsetTop >= 0) {
+        const goUp = player.offsetTop;
+        player.style.top = goUp - 5 + 'px';
+      }
       break;
     case 'ArrowDown':
-      const goDown = player.offsetTop;
-      player.style.top = goDown + 5 + 'px';
+      if (player.offsetTop < maxHeight - 5) {
+        const goDown = player.offsetTop;
+        player.style.top = goDown + 5 + 'px';
+      }
       break;
     default:
       break;
